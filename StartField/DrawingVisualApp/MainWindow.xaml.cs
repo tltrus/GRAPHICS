adbdbdd6ad627f1a63243968836853143bc51d00
tltrus <77125487+tltrus@users.noreply.github.com>
2024-04-20 11:48:52 +0400

    up

---
+++ StartField/DrawingVisualApp/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DrawingVisualApp
+{
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timer;
+        public static Random rnd = new Random();
+        public static double speed;
+        public static int width, height, offsX, offsY;
+
+        DrawingVisual visual;
+        DrawingContext dc;
+        List<Star> stars = new List<Star>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            visual = new DrawingVisual();
+
+            width = (int)g.Width;
+            height = (int)g.Height;
+            offsX = width / 2;
+            offsY = height / 2;
+
+            for (int i = 0; i < 700; i++)
+            {
+                var x = rnd.Next(-width, width);
+                var y = rnd.Next(-height, height);
+                var z = rnd.Next(0, width);
+
+                stars.Add(new Star(x, y, z));
+            }
+
+            timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(TimerTick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+
+            timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e) => Drawing();
+        private void Drawing()
+        {
+            g.RemoveVisual(visual);
+
+            using (dc = visual.RenderOpen())
+            {
+                foreach(var star in stars)
+                {
+                    star.Update();
+                    star.Show(dc);
+                }
+
+                dc.Close();
+                g.AddVisual(visual);
+            }
+        }
+        private void g_MouseMove(object sender, MouseEventArgs e)
+        {
+            var x = e.GetPosition(g).X;
+            speed = Star.Map(x, 0, width, 0, 50);
+        }
+    }
+}
---
+++ StartField/DrawingVisualApp/Star.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+
+namespace DrawingVisualApp
+{
+    class Star
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+        public double pz { get; set; }
+
+        public Star(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            pz = z;
+        }
+
+        public void Update()
+        {
+            z = z - MainWindow.speed;
+            if (z < 1)
+            {
+                z = MainWindow.width;
+                x = MainWindow.rnd.Next(-MainWindow.width, MainWindow.width);
+                y = MainWindow.rnd.Next(-MainWindow.height, MainWindow.height);
+                pz = z;
+            }
+        }
+
+        public void Show(DrawingContext dc)
+        {
+            var sx = Map(x / z, 0, 1, 0, MainWindow.width) + MainWindow.offsX;
+            var sy = Map(y / z, 0, 1, 0, MainWindow.height) + MainWindow.offsY;
+            var r = Map(z, 0, MainWindow.width, 16, 0); ;
+
+            dc.DrawEllipse(Brushes.WhiteSmoke, null, new Point(sx, sy), r, r);
+        }
+
+        public static double Constrain(double n, double low, double high) => Math.Max(Math.Min(n, high), low);
+        public static double Map(double n, double start1, double stop1, double start2, double stop2, bool withinBounds = false)
+        {
+            var newval = (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+            if (!withinBounds)
+            {
+                return newval;
+            }
+            if (start2 < stop2)
+            {
+                return Constrain(newval, start2, stop2);
+            }
+            else
+            {
+                return Constrain(newval, stop2, start2);
+            }
+        }
+    }
+}
