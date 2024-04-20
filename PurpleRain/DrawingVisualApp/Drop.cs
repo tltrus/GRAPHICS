a76b2e6a3584d87a40bab48aefc80cca11ac0efd
tltrus <77125487+tltrus@users.noreply.github.com>
2024-04-20 12:54:59 +0400

    up

---
+++ PurpleRain/DrawingVisualApp/Drop.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+
+namespace DrawingVisualApp
+{
+    class Drop
+    {
+        double x, y, z, yspeed, len;
+        int width, height, minSpeed, maxSpeed;
+
+        public Drop(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            minSpeed = 2;
+            maxSpeed = 8;
+            
+            x = MainWindow.rnd.Next(0, width);
+            y = MainWindow.rnd.Next(-500, -50);
+            z = MainWindow.rnd.Next(0, 20);
+            yspeed = Tools.Map(z, 0, 20, minSpeed, maxSpeed);
+            len = Tools.Map(z, 0, 20, 15, 35);
+        }
+
+        public void Fall()
+        {
+            y += yspeed;
+
+            if (y > height)
+            {
+                y = MainWindow.rnd.Next(-300, 0);
+                yspeed = Tools.Map(z, 0, 20, minSpeed, maxSpeed);
+            }
+        }
+
+        public void Show(DrawingContext dc)
+        {
+            var thick = Tools.Map(z, 0, 20, 1, 3);
+
+            Rect rect = new Rect()
+            {
+                X = x,
+                Y = y,
+                Height = len,
+                Width = thick
+            };
+            dc.DrawRoundedRectangle(null, new Pen(Brushes.LightGray, thick), rect, 1, 1);
+        }
+    }
+}
---
+++ PurpleRain/DrawingVisualApp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+
+namespace DrawingVisualApp
+{
+
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timer;
+        public static Random rnd = new Random();
+        private int width, height;
+
+        DrawingVisual visual;
+        DrawingContext dc;
+        List<Drop> drops = new List<Drop>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            visual = new DrawingVisual();
+
+            width = (int)g.Width;
+            height = (int)g.Height;
+
+            for (int i = 0; i < 500; i++)
+            {
+                drops.Add(new Drop(width, height));          // z
+            }
+
+            timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+
+            timer.Start();
+        }
+
+        private void timerTick(object sender, EventArgs e) => Drawing();
+
+        private void Drawing()
+        {
+            g.RemoveVisual(visual);
+
+            using (dc = visual.RenderOpen())
+            {
+                foreach(var drop in drops)
+                {
+                    drop.Fall();
+                    drop.Show(dc);
+                }
+
+                dc.Close();
+                g.AddVisual(visual);
+            }
+        }
+    }
+}
