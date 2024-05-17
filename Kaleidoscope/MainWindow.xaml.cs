63ce1369dc5707aff3bc9fb14c3fde0aee5cd2a0
tltrus <77125487+tltrus@users.noreply.github.com>
2024-05-17 22:01:08 +0400

    up

---
+++ Kaleidoscope/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp
+{
+    // Based on Coding Challenge #155 — Kaleidoscope Snowflake https://thecodingtrain.com/challenges/155-kaleidoscope-snowflake
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+        WriteableBitmap wb;
+        int width, height;
+
+        int mouseX, pmouseX, mouseY, pmouseY;
+        int offsX, offsY;
+        int thickness = 3;
+        Color color = Colors.LightGray;
+        int xoff;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            width = (int)g.Width; height = (int)g.Height;
+
+            // Для Битмапа
+            wb = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null); g.Source = wb;
+            offsX = width / 2;
+            offsY = height / 2;
+        }
+
+        private void g_MouseMove(object sender, MouseEventArgs e)
+        {
+            pmouseX = mouseX;
+            pmouseY = mouseY;
+
+            mouseX = (int)e.GetPosition(g).X - width / 2;
+            mouseY = (int)e.GetPosition(g).Y - height / 2;
+
+            int mx = mouseX;
+            int my = mouseY;
+            int pmx = pmouseX;
+            int pmy = pmouseY;
+
+
+            if (e.MouseDevice.LeftButton == MouseButtonState.Pressed)
+            {
+                var hu = (byte)Tools.Map(Math.Sin(xoff), -1, 1, 0, 255);
+                xoff++;
+
+                color = Color.FromArgb(150, hu, hu, hu);
+
+                wb.DrawLineAa(mx + offsX, my + offsY, pmx + offsX, pmy + offsY, color, thickness);
+
+                Vector2D mirr = new Vector2D(mx, my * -1);
+                Vector2D pmirr = new Vector2D(pmx, pmy * -1);
+                wb.DrawLineAa((int)mirr.x + offsX, (int)mirr.y + offsY, (int)pmirr.x + offsX, (int)pmirr.y + offsY, color, thickness);
+
+
+                Vector2D mnew = new Vector2D(mx, my);
+                Vector2D pmnew = new Vector2D(pmx, pmy);
+
+                for (int i = 0; i < 5; ++i)
+                {
+                    mnew.Rotate(Math.PI / 3);
+                    pmnew.Rotate(Math.PI / 3);
+
+                    wb.DrawLineAa((int)mnew.x + offsX, (int)mnew.y + offsY, (int)pmnew.x + offsX, (int)pmnew.y + offsY, color, thickness);
+
+                    mirr = new Vector2D(mnew.x, mnew.y * -1);
+                    pmirr = new Vector2D(pmnew.x, pmnew.y * -1);
+                    wb.DrawLineAa((int)mirr.x + offsX, (int)mirr.y + offsY, (int)pmirr.x + offsX, (int)pmirr.y + offsY, color, thickness);
+                }
+            }
+        }
+    }
+}
---
+++ Kaleidoscope/Vector2D.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace WpfApp
+{
+    public class Vector2D
+    {
+        public double x { get; set; }
+
+        public double y { get; set; }
+
+        public Vector2D(double x = 0.0, double y = 0.0)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector2D CopyToVector()
+        {
+            return new Vector2D(x, y);
+        }
+
+        public double[] CopyToArray()
+        {
+            return new double[2] { x, y };
+        }
+
+        public void Set(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector2D Rotate(double a)
+        {
+            Vector2D vector2D = new Vector2D();
+            double num = HeadingRad() + a;
+            double num2 = Mag();
+            x = Math.Cos(num) * num2;
+            y = Math.Sin(num) * num2;
+            return CopyToVector();
+        }
+
+        public double HeadingRad()
+        {
+            return Math.Atan2(y, x);
+        }
+
+        public double HeadingDeg()
+        {
+            double num = HeadingRad();
+            return (num >= 0.0) ? (num * 180.0 / Math.PI) : ((Math.PI * 2.0 + num) * 360.0 / (Math.PI * 2.0));
+        }
+
+        public double Mag()
+        {
+            return Math.Sqrt(MagSq());
+        }
+
+        public double MagSq()
+        {
+            double num = x;
+            double num2 = y;
+            return num * num + num2 * num2;
+        }
+    }
+}
